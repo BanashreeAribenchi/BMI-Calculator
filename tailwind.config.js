fdb42dfbfa9a6eb3c51445b76132b7a4cc7af1aa
Banashree Aribenchi <banashree.aribenchi17@gmail.com>
2023-12-26 23:33:37 +0100

    styling done

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        playfair: "'Playfair Display', serif",
+      },
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/container-queries")],
+};
